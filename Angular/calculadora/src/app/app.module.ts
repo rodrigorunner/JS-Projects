99255268916760c8e8319fe91dbd3126edb7ab7f
rodrigorunner <rodrigoregisrun@gmail.com>
2021-07-13 19:07:58 -0300

    first commit

--- Angular/calculadora/src/app/app.module.ts
+++ Angular/calculadora/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    CalculadoraModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
