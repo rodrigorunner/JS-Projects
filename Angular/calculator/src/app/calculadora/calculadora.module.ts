d6c2f25f85b9e463a839c3e922db0aac537a5cb0
rodrigorunner <rodrigoregisrun@gmail.com>
2021-07-14 08:33:54 -0300

    módulo da calculadora criado

---
+++ Angular/calculator/src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components/calculadora.component';
+
+
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalculadoraModule { }
