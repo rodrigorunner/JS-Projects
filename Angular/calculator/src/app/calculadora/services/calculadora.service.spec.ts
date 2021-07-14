98f355dd2e0aa3d88ef13e74f8d1f5baa329f490
rodrigorunner <rodrigoregisrun@gmail.com>
2021-07-14 09:35:04 -0300

    criando teste unitário da classe de serviço

--- Angular/calculator/src/app/calculadora/services/calculadora.service.spec.ts
+++ Angular/calculator/src/app/calculadora/services/calculadora.service.spec.ts
@@ -13,4 +13,24 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('deve garantir que 1 + 4 = 5', () => {
+    let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  });
+
+  it('deve garantir que 1 - 4 = -3', () => {
+    let sub = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+    expect(sub).toEqual(-3);
+  });
+
+  it('deve garantir que 1 / 4 = 0.25', () => {
+    let div = service.calcular(1, 4, CalculadoraService.DIVISAO);
+    expect(div).toEqual(0.25);
+  });
+
+  it('deve garantir que 1 * 4 = 4', () => {
+    let mult = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+    expect(mult).toEqual(4);
+  });
 });
