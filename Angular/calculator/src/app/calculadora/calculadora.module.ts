10d2b6d00a312fc67727034d588a237fe3d1084e
rodrigorunner <rodrigoregisrun@gmail.com>
2021-07-14 08:45:57 -0300

    exportando o módulo da calculadora

--- Angular/calculator/src/app/calculadora/calculadora.module.ts
+++ Angular/calculator/src/app/calculadora/calculadora.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalculadoraComponent } from './components/calculadora.component';
-
-
-
+import { CalculadoraComponent } from './components/';
 @NgModule({
   declarations: [
     CalculadoraComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
   ]
 })
 export class CalculadoraModule { }
